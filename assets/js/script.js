fbc8a083b069bb2f17048b67da51c170d223d961
emccar22 <e.mccarthy2285@gmail.com>
2021-12-05 14:12:57 -0500

    add to html and link apis and create functions to gather data

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+var currentCity = ""
+var cityFormEl = document.getElementById("city-search")
+var searchInputEl = document.getElementById("city")
+var searchHistoryEl = document.getElementById("search-button-container")
+var currentWeatherConatinerEl = document.getElementById("current-weather-container")
+var currentCityEl = document.getElementById("current-city")
+var currentIcon = document.getElementById("current-icon")
+
+var coordinates = {
+    lat: "",
+    lon: ""
+}
+
+var getWeather = function() {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates.lat + "&lon=" + coordinates.lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=9fb82a034e3a45b2f32f67ba18e0925b"
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+                currentWeatherConatinerEl.className = "border"
+                currentCityEl.textContent = currentCity + " (" +moment().format('L') + ")";
+                var iconCode = data.current.weather[0].icon;
+                var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+                currentIcon.setAttribute("src", iconUrl);
+            })
+        }
+    })
+    
+}
+
+var getLocation = function(city) {
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=9fb82a034e3a45b2f32f67ba18e0925b"
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+                coordinates.lat = data[0].lat
+                coordinates.lon = data[0].lon
+                console.log(coordinates);
+                getWeather();
+            })
+        }
+    })
+    
+}
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var city = searchInputEl.value.trim();
+    currentCity = city;
+
+    if (city) {
+        getLocation(city);
+        searchInputEl.value = "";
+        var historyButtonEl = document.createElement("button");
+        historyButtonEl.className = "btn btn-secondary col-12 mt-1 mb-1";
+        historyButtonEl.setAttribute("value", city);
+        historyButtonEl.textContent = city;
+        searchHistoryEl.appendChild(historyButtonEl);
+    } else {
+        alert("Please Enter a City")
+    }
+}
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+
+
