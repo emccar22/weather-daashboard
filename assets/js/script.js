fca3d1b425a745934091a0a446e4f2cf42f49424
emccar22 <e.mccarthy2285@gmail.com>
2021-12-05 14:43:05 -0500

    add current weather rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-var currentCity = ""
-var cityFormEl = document.getElementById("city-search")
-var searchInputEl = document.getElementById("city")
-var searchHistoryEl = document.getElementById("search-button-container")
-var currentWeatherConatinerEl = document.getElementById("current-weather-container")
-var currentCityEl = document.getElementById("current-city")
-var currentIcon = document.getElementById("current-icon")
+var currentCity = "";
+var cityFormEl = document.getElementById("city-search");
+var searchInputEl = document.getElementById("city");
+var searchHistoryEl = document.getElementById("search-button-container");
+var currentWeatherConatinerEl = document.getElementById("current-weather-container");
+var currentCityEl = document.getElementById("current-city");
+var currentIcon = document.getElementById("current-icon");
+var currentDataListEl = document.getElementById("current-data-list");
 
 var coordinates = {
     lat: "",
@@ -20,9 +21,27 @@ var getWeather = function() {
                 console.log(data)
                 currentWeatherConatinerEl.className = "border"
                 currentCityEl.textContent = currentCity + " (" +moment().format('L') + ")";
+                
                 var iconCode = data.current.weather[0].icon;
+                
                 var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
                 currentIcon.setAttribute("src", iconUrl);
+                
+                var currentTempEl = document.createElement("li");
+                currentTempEl.textContent = "Temp: " + data.current.temp + " \xB0F";
+                currentDataListEl.appendChild(currentTempEl);
+                
+                var currentWindEl = document.createElement("li");
+                currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+                currentDataListEl.appendChild(currentWindEl);
+                
+                var currentHumidityEl = document.createElement("li");
+                currentHumidityEl.textContent = "Humidity: " + data.current.humidity + " %";
+                currentDataListEl.appendChild(currentHumidityEl);
+                
+                var currentUvIndexEl = document.createElement("li");
+                currentUvIndexEl.textContent = "UV Index: " + data.current.uvi;
+                currentDataListEl.appendChild(currentUvIndexEl);
             })
         }
     })
